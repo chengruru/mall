27b8cbcd29fe2cb851d3e2c28006773d1cc966be
chengruru <945562363@qq.com>
2019-03-17 22:57:50 +0800

    购物车模块开发,测试完成

--- src/main/java/com/mmall/dao/CartMapper.java
+++ src/main/java/com/mmall/dao/CartMapper.java
@@ -1,42 +1,38 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Cart;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface CartMapper {
-    /**
-     * 根据主键进行删除
-     * @param id
-     * @return
-     */
     int deleteByPrimaryKey(Integer id);
 
-    /**
-     * 将Cart对象完全插入表中
-     * @param record
-     * @return
-     */
     int insert(Cart record);
 
-    /**
-     * 根据传入对象的某些字段是否为空进行判断,若字段不为空,则将字段放入sql语句中
-     * @param record
-     * @return
-     */
     int insertSelective(Cart record);
 
-    /**
-     * 根据主键查询
-     * @param id
-     * @return
-     */
     Cart selectByPrimaryKey(Integer id);
 
-    /**
-     * 根据主键进行更新
-     * @param record
-     * @return
-     */
     int updateByPrimaryKeySelective(Cart record);
 
     int updateByPrimaryKey(Cart record);
+
+    Cart selectCartByUserIdProductId(@Param("userId") Integer userId, @Param("productId") Integer productId);
+
+    List<Cart> selectCartByUserId(Integer userId);
+
+    int selectCartProductCheckedStatusByUserId(Integer userId);
+
+    int deleteByUserIdProductIds(@Param("userId") Integer userId, @Param("productIdList") List<String> productIdList);
+
+
+    int checkedOrUncheckedProduct(@Param("userId") Integer userId, @Param("productId") Integer productId, @Param("checked") Integer checked);
+
+    int selectCartProductCount(@Param("userId") Integer userId);
+
+
+    List<Cart> selectCheckedCartByUserId(Integer userId);
+
+
 }
